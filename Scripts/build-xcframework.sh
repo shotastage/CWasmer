1248ab2ee01bf6d4c77fd6728367fe1601377728
shotastage <hornet.live.mf@gmail.com>
2022-12-16 01:40:19 +0900

    revise

--- Scripts/build-xcframework.sh
+++ Scripts/build-xcframework.sh
@@ -2,18 +2,23 @@
 
 
 function prepare() {
+    rm -rf arrifacts
     mkdir .wasmer_prebuild
+}
+
+function download() {
     cd .wasmer_prebuild
+    curl -sLJO https://github.com/wasmerio/wasmer/releases/download/v3.1.0/wasmer-darwin-amd64.tar.gz
+    tar -zxvf wasmer-darwin-amd64.tar.gz
+    cd ..
 }
 
 function place() {
-    # mv ./bin/ ../Sources/CWasmer/
-    mv ./include/ ../Sources/CWasmer/
-    mv ./lib/ ../Sources/CWasmer/
+    mv ./.wasmer_prebuild/include/ ./Sources/CWasmer/
+    mv ./.wasmer_prebuild/lib/ ./Sources/CWasmer/
 }
 
 function clean() {
-    cd ..
     rm -rf .wasmer_prebuild
     rm -rf ./Sources/CWasmer/bin
     rm -rf ./Sources/CWasmer/include
@@ -21,21 +26,11 @@ function clean() {
     rm -rf .build/
 }
 
-function build_framework() {
-    xcodebuild archive \
-        -project CWasmer.xcodeproj \
-        -scheme CWasmer \
-        -destination "$1" \
-        -archivePath "$2" \
-        SKIP_INSTALL=NO \
-        BUILD_LIBRARY_FOR_DISTRIBUTION=YES
-}
 
-echo Now under construction...
+
+echo "WARNING: THIS SCRIPT IS NOW UNDER CONSTRUCTION"
 prepare
-curl -sLJO https://github.com/wasmerio/wasmer/releases/download/v3.1.0/wasmer-darwin-amd64.tar.gz
-tar -zxvf wasmer-darwin-amd64.tar.gz
-
+download
 place
 mint run swift-create-xcframework --clean CWasmer --output artifacts --platform ios --platform macos --platform tvos --platform watchos
 clean
