3eb761bffb1338b424c2d2550b403fdcd5913819
shotastage <hornet.live.mf@gmail.com>
2022-12-15 16:37:24 +0900

    add xcodeproj

--- Scripts/build-xcframework.sh
+++ Scripts/build-xcframework.sh
@@ -15,6 +15,20 @@ function place() {
 function clean() {
     cd ..
     rm -rf .wasmer_prebuild
+    rm -rf ./Sources/CWasmer/bin
+    rm -rf ./Sources/CWasmer/include
+    rm -rf ./Sources/CWasmer/lib
+    rm -rf .build/
+}
+
+function build_framework() {
+    xcodebuild archive \
+        -project CWasmer.xcodeproj \
+        -scheme CWasmer \
+        -destination "$1" \
+        -archivePath "$2" \
+        SKIP_INSTALL=NO \
+        BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 }
 
 echo Now under construction...
@@ -23,4 +37,8 @@ curl -sLJO https://github.com/wasmerio/wasmer/releases/download/v3.1.0/wasmer-da
 tar -zxvf wasmer-darwin-amd64.tar.gz
 
 place
+build_framework "generic/platform=iOS" ".archives/CWasmer-iOS"
+build_framework "generic/platform=iOS Simulator" ".archives/CWasmer-iOS-Simulator"
+build_framework "generic/platform=macOS,variant=Mac Catalyst" ".archives/CWasmer-macOS-Catalyst"
+build_framework "generic/platform=macOS" ".archives/CWasmer-macOS"
 clean
